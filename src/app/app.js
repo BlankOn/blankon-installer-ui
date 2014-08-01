2c475755342766b5ee207f9d6be9622fd48ab5ec
Mohammad Anwari <mdamt@mdamt.net>
2014-08-01 10:01:48 +0700

    proper Hello

--- src/app/app.js
+++ src/app/app.js
@@ -1,26 +1,25 @@
-(function(){
-	'use strict';
-	angular.module('Biui', [
-    "ui.router", 
-    "hello"
-  ])
-  .config(function($stateProvider) {
-    $stateProvider
-    .state("hello", {
-        url: "/hello",
-        controller: "HelloCtrl",
-        templateProvider: function($templateCache) {
-          return $templateCache.get("hello/hello.html");
-        }
+'use strict';
+angular.module('Biui', [
+  "ui.router", 
+  "html",
+  "hello"
+])
+.config(function($stateProvider) {
+  $stateProvider
+  .state("hello", {
+      url: "/hello",
+      controller: "HelloCtrl",
+      templateProvider: function($templateCache) {
+        return $templateCache.get("hello/hello.html");
       }
-    )
-  })
-  .run([ "$rootScope", "$state", "$stateParams", 
-    function ($rootScope, $state, $stateParams) {
-      $state.go("hello");
-      $rootScope.started = true;
     }
-  ])
+  )
+})
+.run([ "$rootScope", "$state", "$stateParams", 
+  function ($rootScope, $state, $stateParams) {
+    $state.go("hello");
+    $rootScope.started = true;
+  }
+])
 
 
-})()
--- src/app/hello/hello.js
+++ src/app/hello/hello.js
@@ -1,4 +1,3 @@
 angular.module("hello",[])
 .controller("HelloCtrl", ["$scope", function ($scope){
 }])
-
