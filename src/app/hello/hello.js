bb14613c6845825ed49542f158c3043cb020e345
Herpiko Dwi Aguno <herpiko@gmail.com>
2015-03-05 20:03:04 +0800

    add installation script starter

--- src/app/hello/hello.js
+++ src/app/hello/hello.js
@@ -1,11 +1,17 @@
 angular.module("hello",[])
 .controller("HelloCtrl", [
-    "$scope", "$window", "$rootScope", 
-    function ($scope, $window, $rootScope){
+    "$scope", "$window", "$rootScope", "$timeout", "$interval", 
+    function ($scope, $window, $rootScope, $timeout, $interval){
   $scope.languages = $window.BiLanguage.available();
 
   $scope.setLanguage = function(lang) {
     $rootScope.installationData.lang = lang.id;
     $rootScope.selectedLang = lang.title;
   }
+  /* var tes = $interval(function(){ */
+  /*   console.log("yo"); */
+  /* }, 1000); */
+  /* $timeout(function(){ */
+  /*   $interval.cancel(tes); */
+  /* }, 5000); */
 }])
