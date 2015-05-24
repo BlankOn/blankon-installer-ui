286e17c57a3f578b33a371f5b214d35ba43ffe2d
Herpiko Dwi Aguno <herpiko@gmail.com>
2015-05-25 02:29:49 +0700

    uncomment setLocale function

--- src/app/hello/hello.js
+++ src/app/hello/hello.js
@@ -10,7 +10,7 @@ angular.module("hello",[])
         $rootScope.selectedLang = lang.title;
       }
       // TODO : language selection
-      /* Installation.setLocale("C.UTF-8"); */
+      Installation.setLocale("C.UTF-8");
 
       // initiate partition state early
       $rootScope.partitionState = {
