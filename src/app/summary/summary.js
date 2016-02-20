af389db4818e08548570db6f07671ff2995a357c
Herpiko Dwi Aguno <herpiko@gmail.com>
2016-02-20 21:57:27 +0700

    Fix indentation. Add some important notes

--- src/app/summary/summary.js
+++ src/app/summary/summary.js
@@ -1,6 +1,5 @@
 angular.module("summary",[])
-.controller("SummaryCtrl", [
-    "$scope", "$window", "$rootScope", 
-    function ($scope, $window, $rootScope){
+.controller("SummaryCtrl", ["$scope", "$window", "$rootScope", 
+  function ($scope, $window, $rootScope){
 
 }])
