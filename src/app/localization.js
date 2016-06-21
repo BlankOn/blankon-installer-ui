95b4ac6789570685b74bee0d736644b0109b258b
Hikmah Gumelar <hikmahgumelar@gmail.com>
2016-06-21 16:03:31 +0700

    edited localization.js for language ID and EN

--- src/app/localization.js
+++ src/app/localization.js
@@ -1,8 +1,48 @@
 var en = {
-  welcome_to_program : "Welcome to BlankOn installation program."
+  welcome_to_program : "Welcome to BlankOn installation program.",
+  installation_finish : "Installation Finish.",
+  install_error : "Installation Error Occured.",
+  install_error_info : "Please take a look at installion log : /var/log/blankon-installer.log",
+  install_partition : "Please wait while your sistem is being scanned...",
+  install_partition1 : "Please choose the partition on device below which you would like to install BlankOn into or use advanced partitioning tool for more control.",
+  install_summary : "Installation Summary",
+  install_summary_info : "This is our installation summary. Please proceed if you're agree. Beyond this point, the installer will make changes to your system and you can't go",
+  install_user : "personalization",
+  install_user1 : "Please enter your personalization preference below...",
+  install_user2 : "Computer Name",
+  install_user3 : "Your Name",
+  install_user4 : "Username",
+  install_user5 : "password"
+  install_user6 : "Repeat Password",
+  install_user7 : "Automatically login",
+  install_user8 : "Enforce strong password",
+  install_user9 : "Your password must contain at least 8 letters, a capital, and a numeric",
+  install_user10 : "Please fill all the required form."
+
+
+
 }
 
 var id = {
-  welcome_to_program : "Selamat datang di program pemasang BlankOn."
+  welcome_to_program : "Selamat datang di program pemasang BlankOn.",
+  installation_finish : "Pemasangan Selesai.",
+  install_error : "Terjadi Kesalahan Pemasangan.",
+  install_error_info : "Silakan lihat di log pemasangan : /var/log/blankon-installer.log",
+  install_partition : "Silahkan menunggu system sedang mencari...",
+  install_partition1 : "Silahkan pilih partisi pada perangkat di bawah ini yang Anda ingin memasang BlankOn ke dalam atau menggunakan alat partisi canggih untuk kontrol lebih.",
+  install_summary : "Ringkasan Pemasangan",
+  install_summary_info : "Ini adalah ringkasan pemasangan kita. Lanjutkan jika Anda setuju. pada langkah selanjutnya Pemasang akan membuat perubahan pada sistem Anda dan Anda tidak bisa membatalkannya",
+  install_user  : "Personalisasi",
+  install_user1 : "Masukkan preferensi personalisasi Anda di bawah ini ...",
+  install_user2 : "Nama Komputer",
+  install_user3 : "Nama Kamu",
+  install_user4 : "Nama Pengguna",
+  install_user5 : "Kata Sandi",
+  install_user6 : "Ulangi Kata Sandi",
+  install_user7 : "Masuk Secara Otomatis",
+  install_user8 : "Membuat Kata Sandi Yang Kuat",
+  install_user9 : "Kata Sandi Anda harus berisi minimal 8 huruf , modal , dan numerik.",
+  install_user10 : "Silahkan mengisi semua formulir yang diperlukan."
+
 
 }
