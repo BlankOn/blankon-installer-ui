d03e9224096b73e6fcf0d84a4c73c782e6e79011
Mohammad Anwari <mdamt@mdamt.net>
2016-06-26 17:15:45 +0700

    Use correct language codes

--- src/app/hello/hello.js
+++ src/app/hello/hello.js
@@ -1,15 +1,14 @@
 angular.module("hello",[])
-.controller("HelloCtrl", ["$scope", "$window", "$rootScope", "$translate", 
+.controller("HelloCtrl", ["$scope", "$window", "$rootScope", "$translate",
   function ($scope, $window, $rootScope, $translate){
-    
+
     $scope.languages = $window.BiLanguage.available();
     $scope.setLanguage = function(lang) {
       console.log(lang);
       $rootScope.installationData.lang = lang.id;
       $rootScope.selectedLang = lang.title;
       $translate.use(lang.id);
+      Installation.setLocale(lang.id);
     }
-    // TODO : language selection
-    /* Installation.setLocale("C.UTF-8"); */
 
 }])
